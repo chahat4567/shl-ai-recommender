d94e3bbc1458a81c02e13c8bba64086e847d1244
chahat4567 <133643575+chahat4567@users.noreply.github.com>
2026-05-19 03:46:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import os
+import google.generativeai as genaifrom fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
